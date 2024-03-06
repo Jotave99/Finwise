2789938704d1c5719b5f0a887c211b7877987e07
Jotave99 <joaobva2016@gmail.com>
2024-03-05 21:06:00 -0300

    Adding Login page

---
+++ src/model/Login/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { Container, FormContainer, Title, Form, Input, Button, ErrorMessage } from './style';
+
+const Login: React.FC = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const [error, setError] = useState<string | null>(null); // State para armazenar mensagens de erro
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3001/auth/login', formData);
+      const { token } = response.data; // Obter o token da resposta
+      localStorage.setItem('token', token); // Armazenar o token no localStorage
+      navigate('/home'); // Redirecionar para a página inicial
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+      setError('Usuário ou senha incorretos'); // Definir mensagem de erro
+    }
+  };
+
+  return (
+    <Container>
+      <FormContainer>
+        <Title>Fazer Login</Title>
+        <Form onSubmit={handleSubmit}>
+          <Input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} />
+          <Input type="password" name="password" placeholder="Senha" value={formData.password} onChange={handleChange} />
+          <Button type="submit">Login</Button>
+          {error && <ErrorMessage>{error}</ErrorMessage>} {/* Exibir mensagem de erro, se houver */}
+        </Form>
+      </FormContainer>
+    </Container>
+  );
+};
+
+export default Login;
